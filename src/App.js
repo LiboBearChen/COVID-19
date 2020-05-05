1c981fd1da1bab1124f1544e0ca95020c20e3f0b
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-05-04 21:36:09 -0400

    update

--- src/App.js
+++ src/App.js
@@ -7,12 +7,20 @@ class App extends React.Component{
 
     state={
         data:{},
+        country:'',
     }
 
     async componentDidMount(){
         const fetchedData=await fetchData();
 
         this.setState({data:fetchedData});
+    }
+
+    handleCountryChange=async(country)=>{
+        const fetchedData=await fetchData(country);
+
+        console.log(fetchedData);
+
     }
 
     render(){
@@ -22,7 +30,7 @@ class App extends React.Component{
         return(
             <div className={styles.container}>
                 <Cards data={data}/>
-                <CountryPicker />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
                 <Chart />
             </div>
         )
